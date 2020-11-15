8c37ce1ec36d88310f13c4d8b2ba453781ee7e5b
Maximilian Klein <kontakt@kleindev.de>
2020-11-15 14:49:56 +0100

    Added index.php

--- index.php
+++ index.php
@@ -1,28 +1,41 @@
+<html>
+<head>
+    <title>Discord Export Chat Viewer</title>
+</head>
+<body>
+<form action="index.php" method="get">
+    <input name="search" placeholder="Search...">
+    <button type="submit">Search</button>
+</form>
+</body>
+</html>
+
 <?php
 require_once "DiscordExportAnalyzer.php";
 
-if (!is_file("package")){
-    if (is_file("package.zip")){
-
-    } else die("The Discord-data-export package must be on the same directory as this script, please ");
+if (!is_dir(__DIR__.DIRECTORY_SEPARATOR."package")) {
+    if (is_file(__DIR__.DIRECTORY_SEPARATOR."package.zip")) {
+        $zip = new ZipArchive();
+        $zip->open(__DIR__.DIRECTORY_SEPARATOR."package.zip");
+        $zip->extractTo(__DIR__.DIRECTORY_SEPARATOR."package");
+    } else die("The Discord-data-export package must be on the same directory as this script, also it must be named \"package\"!");
 }
+$da = new DiscordExportAnalyzer(__DIR__.DIRECTORY_SEPARATOR."package");
 
-$da = new DiscordExportAnalyzer("package");
 
-
-if (isset($_GET["search"])){
+if (isset($_GET["search"])) {
     $searchResults = $da->searchForMessage(html_entity_decode($_GET["search"]));
-    echo $da->getTableWithSearchResults($searchResults);
-}
-
-if (isset($_GET["openChat"])){
-    if(isset($_GET["select"]))
-        echo $da->getTableWithChatContent($_GET["openChat"], explode(",",$_GET["select"]));
+    echo $da->getTableWithSearchResults($searchResults, html_entity_decode($_GET["search"]));
+    return;
+} else if (isset($_GET["openChat"])) {
+    if (isset($_GET["select"]))
+        echo $da->getTableWithChatContent($_GET["openChat"], explode(",", $_GET["select"]));
     else echo $da->getTableWithChatContent($_GET["openChat"]);
+    return;
 } else {
-
-//    echo $da->getTableWithChats();
+    echo $da->getTableWithChats();
 }
+?>
 
 
 
